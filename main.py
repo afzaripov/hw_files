12b6265b0be50806062a504d3caa43e0f57af7eb
afzaripov <afzaripow@gmail.com>
2024-12-25 23:39:50 +0500

    Task3. Union files

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from pprint import pprint
+import os
 
 def main():
     cook_book = get_cook_book()
+    pprint(cook_book)
+
     shop_list = get_shop_list_by_dishes(cook_book, ['Фахитос', 'Омлет'], 2)
     pprint(shop_list)
 
+    union_files("task3_examples")
 
+
+# Task 1
 def get_cook_book():
     cook_book = {}
     with open("recipes.txt", encoding="utf-8") as file:
@@ -16,7 +22,7 @@ def get_cook_book():
             #  Пустая строка
             if data[i] == "":
                 i += 1
-                continue
+
             # Название блюда
             dish_name = data[i]
             i+=1
@@ -36,7 +42,7 @@ def get_cook_book():
             
     return cook_book
 
-
+# Task 2
 def get_shop_list_by_dishes(cook_book : dict, dishes : list, person_count : int):
     shop_list = {}
     for dish in dishes:
@@ -45,13 +51,32 @@ def get_shop_list_by_dishes(cook_book : dict, dishes : list, person_count : int)
                 name = ingredient["ingredient_name"]
                 measure = ingredient["measure"]
                 quantity = int(ingredient["quantity"]) * person_count
-            
                 if name in shop_list:
                     shop_list[name]["quantity"] += quantity
                 else:
                     shop_list[name] = {"measure": measure, "quantity": quantity} 
 
     return shop_list
+
+#Task 3
+def union_files(folder_path: str):
+    # {кол-во строк: [имя файла, [строка1, строка2 ...]}
+    files = {}
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+    
+        with open(file_path, encoding="utf-8") as file:
+            data = file.readlines()
+            files[len(data)] = [file_name, data]
+
+
+    with open("task3.txt", "a", encoding="utf-8") as file:
+        for length in sorted(files.keys()):
+            file_name, content = files[length]
+            file.write(f"{file_name}\n")
+            file.write(f"{length}\n")
+            file.writelines(content)
+            file.write("\n")
 
 
 if __name__ == "__main__":
