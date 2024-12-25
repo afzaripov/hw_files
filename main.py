106ec2efe225d2ce5527688ec58f70254e2260ab
afzaripov <afzaripow@gmail.com>
2024-12-25 21:50:08 +0500

    task1. Create book_cook from recipes.txt

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+from pprint import pprint
+
 def main():
-    pass
+    cook_book = get_cook_book()
+    pprint(cook_book)
+
+
+def get_cook_book():
+    cook_book = {}
+    with open("recipes.txt", encoding="utf-8") as file:
+        data = [str_.strip() for str_ in file.readlines()]
+        
+        i = 0
+        while i < len(data):
+            #  Пустая строка
+            if data[i] == "":
+                i += 1
+                continue
+            # Название блюда
+            dish_name = data[i]
+            i+=1
+
+            # Кол-во ингредиентов
+            ingredients_counter = int(data[i])
+            i += 1
+            
+            # Список со словарями ингредиентов и их параметрами
+            ingredients = []
+            for _ in range(ingredients_counter):
+                ingredients_info = data[i].split("|")
+                ingredients.append({"ingredient_name" : ingredients_info[0], "quantity" : ingredients_info[1], "measure" : ingredients_info[2]})
+                i += 1
+            
+            cook_book[dish_name] = ingredients
+            
+    return cook_book
 
 
 
